9a18badc6269bca76ebcf466a0ae760db42fb766
Happiness549 <mhlongohappiness880@gmail.com>
2025-03-13 09:55:35 +0200

    Added a function that will determine who the winner is

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ function getHumanChoice () {
     }
 
 }
+
+
+function determineWinner (userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return "It is a tie";
+    } 
+    
+    if (
+        (userChoice === "rock" && computerChoice ==="scissors") ||
+    (userChoice === "paper" &&computerChoice === "rock") ||
+    (userChoice ==="scissors" && computerChoice ==="paper")
+) {
+       return "You win";
+       
+    } else {
+        return "Computer wins!";
+    }
+}
